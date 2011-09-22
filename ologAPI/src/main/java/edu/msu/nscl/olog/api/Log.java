b27ca5a0c83ff7c668ac9ba9c4377ef362de44ea
Kunal Shroff <shroffk@bnl.gov>
2011-09-22 14:36:50 -0400

    changing the internal implementation on how the tags, properties, logbooks and attachments are saved.

--- ologAPI/src/main/java/edu/msu/nscl/olog/api/Log.java
+++ ologAPI/src/main/java/edu/msu/nscl/olog/api/Log.java
@@ -3,66 +3,74 @@ package edu.msu.nscl.olog.api;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
  * @author Eric Berryman taken from shroffk
  * 
  */
-public class Log  implements Comparable<Log> {
-        private final Long id;
-        private final String owner;
+public class Log implements Comparable<Log> {
+	private final Long id;
+	private final String owner;
 	private final String subject;
 	private final String description;
-        private final String level;
-        private final Date createdDate;
-        private final Date modifiedDate;
-        private final int version;
-	private final Set<Tag> tags;
-	private final Set<Logbook> logbooks;
-        private final Set<Attachment> attachments;
-        private final Set<Property> properties;
+	private final String level;
+	private final Date createdDate;
+	private final Date modifiedDate;
+	private final int version;
+	// private final Set<Tag> tags;
+	// private final Set<Logbook> logbooks;
+	// private final Set<Attachment> attachments;
+	// private final Set<Property> properties;
+
+	private final Map<String, Tag> tags;
+	private final Map<String, Logbook> logbooks;
+	private final Map<String, Attachment> attachments;
+	private final Map<String, Property> properties;
 
 	Log(XmlLog log) {
-                this.id = log.getId();
-                this.owner = log.getOwner();
+		this.id = log.getId();
+		this.owner = log.getOwner();
 		this.subject = log.getSubject();
 		this.description = log.getDescription();
-                this.level = log.getLevel();
-                this.createdDate = log.getCreatedDate();
-                this.modifiedDate = log.getModifiedDate();
-                this.version = log.getVersion();
-		Set<Tag> newTags = new HashSet<Tag>();
+		this.level = log.getLevel();
+		this.createdDate = log.getCreatedDate();
+		this.modifiedDate = log.getModifiedDate();
+		this.version = log.getVersion();
+		Map<String, Tag> newTags = new HashMap<String, Tag>();
 		for (XmlTag tag : log.getXmlTags().getTags()) {
-			newTags.add(new Tag(tag));
+			newTags.put(tag.getName(), new Tag(tag));
 		}
-		this.tags = Collections.unmodifiableSet(newTags);
-		Set<Logbook> newLogbooks = new HashSet<Logbook>();
+		this.tags = Collections.unmodifiableMap(newTags);
+		Map<String, Logbook> newLogbooks = new HashMap<String, Logbook>();
 		for (XmlLogbook logbook : log.getXmlLogbooks().getLogbooks()) {
-			newLogbooks.add(new Logbook(logbook));
+			newLogbooks.put(logbook.getName(), new Logbook(logbook));
 		}
-		this.logbooks = Collections.unmodifiableSet(newLogbooks);
-                Set<Attachment> newAttachments = new HashSet<Attachment>();
-                for (XmlAttachment attachment : log.getXmlAttachments().getAttachments()) {
-                        newAttachments.add(new Attachment(attachment));
-                }
-                this.attachments = Collections.unmodifiableSet(newAttachments);
-                Set<Property> newProperties = new HashSet<Property>();
-                for (XmlProperty property : log.getXmlProperties().getProperties()) {
-                        newProperties.add(new Property(property));
-                }
-                this.properties = Collections.unmodifiableSet(newProperties);
+		this.logbooks = Collections.unmodifiableMap(newLogbooks);
+		Map<String, Attachment> newAttachments = new HashMap<String, Attachment>();
+		for (XmlAttachment attachment : log.getXmlAttachments()
+				.getAttachments()) {
+			newAttachments.put(attachment.getUri(), new Attachment(attachment));
+		}
+		this.attachments = Collections.unmodifiableMap(newAttachments);
+		Map<String, Property> newProperties = new HashMap<String, Property>();
+		for (XmlProperty property : log.getXmlProperties().getProperties()) {
+			newProperties.put(property.getName(), new Property(property));
+		}
+		this.properties = Collections.unmodifiableMap(newProperties);
 
 	}
 
-        public Long getId() {
-            return id;
-        }
+	public Long getId() {
+		return id;
+	}
 
-        public String getOwner() {
-            return owner;
-        }
+	public String getOwner() {
+		return owner;
+	}
 
 	public String getSubject() {
 		return subject;
@@ -72,37 +80,39 @@ public class Log  implements Comparable<Log> {
 		return description;
 	}
 
-        public String getLevel() {
+	public String getLevel() {
 		return level;
 	}
 
-        public Date getCreatedDate() {
-                return createdDate;
-        }
+	public Date getCreatedDate() {
+		return createdDate;
+	}
 
-        public Date getModifiedDate() {
-                return modifiedDate;
-        }
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
 
-        public int getVersion() {
-                return version;
-        }
+	public int getVersion() {
+		return version;
+	}
 
 	public Collection<Tag> getTags() {
-		return tags;
+		return tags.values();
 	}
 
 	public Collection<Logbook> getLogbooks() {
-		return logbooks;
+		return logbooks.values();
 	}
-        // @deprecated  not really deprecated, but javadoc doesn't have future
-        @Deprecated public Collection<Attachment> getAttachments() {
-                return attachments;
-        }
 
-        public Collection<Property> getProperties() {
-                return properties;
-        }
+	// @deprecated not really deprecated, but javadoc doesn't have future
+	@Deprecated
+	public Collection<Attachment> getAttachments() {
+		return attachments.values();
+	}
+
+	public Collection<Property> getProperties() {
+		return properties.values();
+	}
 
 	/*
 	 * (non-Javadoc)
@@ -116,17 +126,19 @@ public class Log  implements Comparable<Log> {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	/*
 	 * (non-Javadoc)
-	 *
+	 * 
 	 * @see java.lang.Object#compareTo(java.lang.Object)
 	 */
-        @Override
-        public int compareTo(Log obj) throws ClassCastException {
-            if (!(obj instanceof Log))
-                throw new ClassCastException("A Log object expected.");
-            return (int) (this.id - ((Log) obj).getId());
-        }
+	@Override
+	public int compareTo(Log obj) throws ClassCastException {
+		if (!(obj instanceof Log))
+			throw new ClassCastException("A Log object expected.");
+		return (int) (this.id - ((Log) obj).getId());
+	}
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -146,19 +158,20 @@ public class Log  implements Comparable<Log> {
 				return false;
 		} else if (!id.equals(other.id)) {
 			return false;
-                } //else if (version != other.version)
-                  //      return false;
+		} // else if (version != other.version)
+			// return false;
 		return true;
 	}
 
 	/*
 	 * (non-Javadoc)
-	 *
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
-		return "Log#"+id+":v."+version+" [subject=" + subject + ", description=" + description + "]";
+		return "Log#" + id + ":v." + version + " [subject=" + subject
+				+ ", description=" + description + "]";
 	}
 
 }
